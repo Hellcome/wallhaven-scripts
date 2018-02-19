72f57fcdfd60557bb841156255197514a8b41b34
Hellcome <36553400+Hellcome@users.noreply.github.com>
2018-02-19 22:40:54 +0300

    Update wallrandom.sh

--- wallrandom.sh
+++ wallrandom.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+##
 
 dir=$HOME/bin #your scripts dir
 picURL=https://alpha.wallhaven.cc/wallpapers/full
 
+
 ##Wallhaven setings
 site=https://alpha.wallhaven.cc
-sorting=favorites
-purity=100
-categories=011
-attleast=1920x1080
+
+sorting=toplist #Relevance / Random / Date Added / Views / Favorites / Toplist
+purity=100 
+categories=011 # 100 - General / 110 General + Anime / 111 ALL / 011 Anime + People / 010 - Anime / 001 - People / 111 - People + General
+attleast=1920x1080 # your resolution
+topRange=6M # sort by date 1d / 3d / 1w / 1M / 3M / 6M / 12M / 1y
+random=$(shuf -i1-20 -n1) # specify the desired number of pages. Сhane this -i1-20
+
+siteURL="$site/search?q=&categories=$categories&purity=$purity&atleast=$atleast&topRange=$topRange&sorting=$sorting&order=desc&page=$random"
 
 # time flag
 
@@ -23,38 +30,32 @@ done
 
 while true ; do
 
-html=$(curl -s "$site/search?q=&categories=$categories&purity=$purity&atleast=$atleast&sorting=$sorting&order=desc&page=$(shuf -i1-5 -n1)" | grep -o '<a class="preview" href="https://alpha.wallhaven.cc/wallpaper/[0-9]*"' | sed  's .\{24\}  ' | tr -d '"' | awk 'BEGIN { srand() }
-{ l[NR]=$0 } END { print l[int(rand() * NR + 1)] }' )
 
+html=$(curl -s "$siteURL" | grep -o '<a class="preview" href="https://alpha.wallhaven.cc/wallpaper/[0-9]*"' | sed  's .\{24\}  ' | tr -d '"' | awk 'BEGIN { srand() }
+{ l[NR]=$0 } END { print l[int(rand() * NR + 1)] }'  )
 
 wall=$(curl -s $html | grep -Po '(?<=src=")[^"]*(jpg|png)' | awk 'NR!~/^(1|2)$/' | cut -c 43-)
-
 
 cd $dir
 
 wget -q "$picURL/$wall"
 
-
-
 rm=$(head -1 $HOME/bin/wall.txt)
 rm -f $rm
 rm -f wall.txt
 
-
-
 gsettings set org.gnome.desktop.background picture-uri "file:///$dir/$wall"
 
 echo $wall > wall.txt
+
 
 ## Time functions
 
 	if [[ -z "${time}" ]] ; then #If time is not set, break loop (so the script finishes).
 		break
 	fi
-	if test ${time} -lt 0 ; then #If time is negative (illegal), break loop (so the script finishes).
-		echo "ERROR: Time (-t) needs to be set to a positive number (in seconds). EXITING!"
-		break
-	fi
 	sleep $time
   
 done
+
+exit
