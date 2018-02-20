79a4fbdc494d9b21c8aa9c8422956f732512ebd4
Hellcome <36553400+Hellcome@users.noreply.github.com>
2018-02-20 21:58:31 +0300

    Update savethis.sh

--- savethis.sh
+++ savethis.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+RED="tput setaf 1"
+GREEN="tput setaf 14"
 dir=$HOME/bin #your script dir
 pic=$(head -1 $dir/wall.txt)
 folder=$HOME/Pictures/wallhaven #your wallpapers dir
 
-cp $dir/$pic $folder/$pic
+if [ $(find $folder -iname $pic -print) ]  
+   then $RED ; echo "$pic already save"
+   else
+   cp $dir/$pic $folder/$pic 
+   $GREEN ; echo "$pic saved to $folder/$pic"
+fi
 
-echo $pic saved to $folder/$pic 
+exit 0 
