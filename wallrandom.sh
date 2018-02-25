a6dd7641e821a1dff546157deaf98a6c21958f08
Hellcome <36553400+Hellcome@users.noreply.github.com>
2018-02-25 21:57:13 +0300

    Add serch options and small fixes

--- wallrandom.sh
+++ wallrandom.sh
@@ -2,6 +2,7 @@
 
 ##
 
+
 #Help function
 function HELP {
   echo "Command line switches are optional. The following switches are recognized."
@@ -10,7 +11,8 @@ function HELP {
   echo "$blue-c$nc -- Range options$blue-c$nc$blue | General →  g, general | General and Anime →  ga | General and People →  gp | all →  all | Anime and People →  ap | Anime →  a, anime | People →  p, people$nc"    
   echo "$blue-t$nc -- Time options, use$blue 10 | 10m | 10h$nc"
   echo "$blue-p$nc -- Purity options, use$blue 100 - SWF | 011 - Scetchy | 001 -NSFW | 110 - SFW + Scetchy | 101 - Scetchy + NSFW | 111 - ALL (For NSFW need login, change lines in Needed for NSFW block)"
-  echo "examle$blue ./wallrandom.sh -s toplist -r 1M -c anime -t 10m -p 100$nc"
+  echo "$blue-f$nc -- Search options"
+  echo "examle$blue ./wallrandom.sh -s toplist -r 1M -c anime -f girl -t 10m -p 100$nc"
   exit 1
 }
 ##
@@ -49,6 +51,7 @@ red="$(tput setaf 1)"
 green="$(tput setaf 14)"
 blue="$(tput setaf 4)"
 nc="$(tput sgr0)"
+search=""
 
 ##Wallhaven setings
 site=https://alpha.wallhaven.cc
@@ -58,26 +61,15 @@ categories=111 # 100 - General / 110 - General + Anime / 101 - General + People 
 attleast=1920x1080 # your resolution
 topRange=6M # sort by date 1d / 3d / 1w / 1M / 3M / 6M / 12M / 1y
 random=$(shuf -i1-5 -n1) # specify the desired number of pages. Сhane this -i1-5
-
+search=''
 ## Flags
+## Functions 
 
-while getopts ":t:s:r:c:p:h:" o; do 
-  case $o in
-    t) time=$OPTARG;;
-    s) sorting=$OPTARG
-        if [ $sorting != favorites ] && [ $sorting != toplist ] && [ $sorting != random ] && [ $sorting != date_added ] && [ $sorting != views ] && [ $sorting != relevance ] ; then
-        echo "$red invalid parameter ($sorting)$nc use$blue favorites | random | date_added | views | toplist | revelance$nc" ; exit 0 
-        else 
-        echo "sorting set to $blue$sorting$nc"
-        fi ;;
-    r) topRange=$OPTARG
-       if [ $topRange != 1d ] && [ $topRange != 3d ] && [ $topRange != 1w ] && [ $topRange != 1M ] && [ $topRange != 3M ] && [ $topRange != 6M ] && [ $topRange != 1y ] ; then
-        echo "$red invalid parameter ($topRange)$nc use$blue 1d | 3d | 1w | 1M | 3M | 6M | 1y$nc" ; exit 0 
-        else 
-        echo "topRange set to $blue$topRange$nc" 
-        fi ;;
-    c) categories=$OPTARG
-        if [ $categories != general ] && [ $categories != g ] && [ $categories != ga ] && [ $categories != gp ] && [ $categories != all ] && [ $categories != ap ] && [ $categories != pepople ] && [ $categories != p ] && [ $categories != anime ] && [ $categories != a ] ; then
+function cat {
+
+categories=$OPTARG
+
+if [ $categories != general ] && [ $categories != g ] && [ $categories != ga ] && [ $categories != gp ] && [ $categories != all ] && [ $categories != ap ] && [ $categories != pepople ] && [ $categories != p ] && [ $categories != anime ] && [ $categories != a ] ; then
         echo -e $red'\n'invalid parameter $categories$nc$blue'\n'•General →  g, general "\n"•General and Anime →  ga "\n"•General and People →  gp '\n'•all →  all '\n'•Anime and People →  ap '\n'•Anime →  a, anime '\n'•People →  p, people$nc'\n' ; exit 0 
         fi 
         if [ $categories == anime ] ; then 
@@ -110,19 +102,67 @@ while getopts ":t:s:r:c:p:h:" o; do
         if [ $categories == p ] ; then 
         categories=001 ; echo categories set to$blue People$nc
         fi
-        ;;
-    p) purity=$OPTARG
+
+} #categories
+
+function range {
+topRange=$OPTARG
+       if [ $topRange != 1d ] && [ $topRange != 3d ] && [ $topRange != 1w ] && [ $topRange != 1M ] && [ $topRange != 3M ] && [ $topRange != 6M ] && [ $topRange != 1y ] ; then
+        echo "$red invalid parameter ($topRange)$nc use$blue 1d | 3d | 1w | 1M | 3M | 6M | 1y$nc" ; exit 0 
+        else 
+        echo "topRange set to $blue$topRange$nc" 
+        fi 
+} #Toprange
+
+function sorti {
+
+sorting=$OPTARG
+        if [ $sorting != favorites ] && [ $sorting != toplist ] && [ $sorting != random ] && [ $sorting != date_added ] && [ $sorting != views ] && [ $sorting != relevance ] ; then
+        echo "$red invalid parameter ($sorting)$nc use$blue favorites | random | date_added | views | toplist | revelance$nc" ; exit 0 
+        else 
+        echo "sorting set to $blue$sorting$nc"
+        fi
+} #sorting
+
+function pur {
+purity=$OPTARG
         if [ $purity != 001 ] && [ $purity != 100 ] && [ $purity != 010 ] && [ $purity != 111 ] && [ $purity != 011 ] && [ $purity != 101 ]  ; then
         echo "$red invalid parameter ($purity)$nc use$blue 100 - SWF | 011 - Scetchy | 001 -NSFW | 110 - SFW + Scetchy | 101 - Scetchy + NSFW | 111 - ALL (For NSFW need login, change lines in Needed for NSFW block)$nc" ; exit 0 
         else 
         echo "purity set to $blue$purity$nc"
-        fi ;;
-    h)  HELP ;; 
-    *)  HELP ;;
-    
-    
+        fi 
+} #purity
+
+function loop {
+time=$OPTARG
+echo "auto change set to $blue$time$nc" 
+} #time
+
+
+function srch {
+search=$OPTARG
+echo "search $blue$search$nc"
+} #time
+
+####Functions#####
+
+####Flags#########
+
+
+while getopts ":t:s:r:c:p:h:z:f:" o; do 
+  case $o in
+    t) loop ;; 
+    s) sorti ;;
+    r) range  ;;
+    c) cat ;;
+    p) pur ;;
+    h) HELP ;;
+    z) page ;;
+    f) srch ; shift ;;
+    *) HELP ;;       
 esac
 done
+
 
 while true ; do
 
@@ -136,7 +176,7 @@ login "$USER" "$PASS"
 
 echo "$(tput setaf 4)successful login $USER$nc"
 
-html=$(curl -s --cookie cookies.txt "$site/search?q=&categories=$categories&purity=$purity&atleast=$atleast&topRange=$topRange&sorting=$sorting&order=desc&page=$random" | grep -o '<a class="preview" href="https://alpha.wallhaven.cc/wallpaper/[0-9]*"' | sed  's .\{24\}  ' | tr -d '"' | awk 'BEGIN { srand() }
+html=$(curl -s --cookie cookies.txt "$site/search?q=$search&categories=$categories&purity=$purity&atleast=$atleast&topRange=$topRange&sorting=$sorting&order=desc&page=$random" | grep -o '<a class="preview" href="https://alpha.wallhaven.cc/wallpaper/[0-9]*"' | sed  's .\{24\}  ' | tr -d '"' | awk 'BEGIN { srand() }
 { l[NR]=$0 } END { print l[int(rand() * NR + 1)] }' )
 
 
@@ -160,7 +200,7 @@ echo "$blue$wall$nc set to wallpaper";echo "$blue$html$nc"
 else 
 
  
-html=$(curl -s "$site/search?q=&categories=$categories&purity=$purity&atleast=$atleast&topRange=$topRange&sorting=$sorting&order=desc&page=$random" | grep -o '<a class="preview" href="https://alpha.wallhaven.cc/wallpaper/[0-9]*"' | sed  's .\{24\}  ' | tr -d '"' | awk 'BEGIN { srand() }
+html=$(curl -s "$site/search?q=$search&categories=$categories&purity=$purity&atleast=$atleast&topRange=$topRange&sorting=$sorting&order=desc&page=$random" | grep -o '<a class="preview" href="https://alpha.wallhaven.cc/wallpaper/[0-9]*"' | sed  's .\{24\}  ' | tr -d '"' | awk 'BEGIN { srand() }
 { l[NR]=$0 } END { print l[int(rand() * NR + 1)] }'  )
 
 wall=$(curl -s "$html" | grep -Po '(?<=src="//wallpapers.wallhaven.cc/wallpapers/full/)[^"]*(jpg|png|gif|bmp|jpeg)')
@@ -176,7 +216,7 @@ rm -f wall.txt
 gsettings set org.gnome.desktop.background picture-uri "file:///$dir/$wall"
 
 echo $wall > wall.txt
-echo "$blue$wall$nc set to wallpaper";echo "$blue$html$nc"
+echo "$blue$wall$nc set to wallpaer";echo "$blue$html$nc"
 
 fi
 ## Time functions
